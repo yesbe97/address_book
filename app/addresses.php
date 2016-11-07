0d8751b710d1bb1ea435b955b79066168b3b617f
yesbe97 <vandrielnicky@gmail.com>
2016-11-07 14:43:12 +0100

    DB setup

---
+++ app/addresses.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class addresses extends Model
+{
+    protected $fillable = [
+        "first_name",
+        "last_name",
+        "street_name",
+        "home_number",
+        "phone_number",
+        "created_at",
+        "updated_at"
+    ];
+}
