73592a62f5b4e7169232235defc69427174755b0
yesbe97 <vandrielnicky@gmail.com>
2016-11-07 14:10:12 +0100

    basic setup

---
+++ app/Http/Controllers/defaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class defaultController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function edit()
+    {
+        
+    }
+
+    public function remove()
+    {
+        
+    }
+}
